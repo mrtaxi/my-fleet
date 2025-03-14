b6e082c44c5e0cccfebfcd008edf09c85adf1154
mleonidov <maxim.leonidov@danads.se>
2025-03-14 18:31:25 +0200

    fix syntax

--- services/letsencrypt/renew_certs.sh
+++ services/letsencrypt/renew_certs.sh
@@ -3,6 +3,5 @@
 certbot renew --noninteractive
 
 
-cp /etc/letsencrypt/live/$YOURDOMAIN/fullchain.pem /secrets/certificate.pem
-cp /etc/letsencrypt/live/$YOURDOMAIN/privkey.pem /secrets/certificate.key
-
+cp /etc/letsencrypt/live/${YOURDOMAIN}/fullchain.pem /secrets/certificate.pem
+cp /etc/letsencrypt/live/${YOURDOMAIN}/privkey.pem /secrets/certificate.key
