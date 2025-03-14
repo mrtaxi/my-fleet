339851604b41bb2ad8988783a3dd3d993756a621
mleonidov <maxim.leonidov@danads.se>
2025-03-14 18:52:39 +0200

    change creating script logic

--- services/letsencrypt/renew_certs.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-certbot renew --noninteractive
-
-
-cp /etc/letsencrypt/live/${YOURDOMAIN}/fullchain.pem /secrets/certificate.pem
-cp /etc/letsencrypt/live/${YOURDOMAIN}/privkey.pem /secrets/certificate.key
